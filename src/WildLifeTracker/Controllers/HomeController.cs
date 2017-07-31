f4cd987355f68cc6ac52a2ce79ab945a9e5bc755
David Wilson and Pete Lazuran <david+pete@>
2017-07-31 11:48:08 -0700

    add CRUD functionality to species class

---
+++ src/WildLifeTracker/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WildLifeTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WildLifeTracker.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private WildlifeTrackerContext db = new WildlifeTrackerContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        { 
+            return View(db.Species.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisSpecies = db.Species.FirstOrDefault(species => species.speciesId == id);
+            return View(thisSpecies);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Species species)
+        {
+            db.Species.Add(species);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisSpecies = db.Species.FirstOrDefault(species => species.speciesId == id);
+            return View(thisSpecies);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Species species)
+        {
+            db.Entry(species).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var thisSpecies = db.Species.FirstOrDefault(species => species.speciesId == id);
+            return View(thisSpecies);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisSpecies = db.Species.FirstOrDefault(species => species.speciesId == id);
+            db.Species.Remove(thisSpecies);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/WildLifeTracker/Models/Species.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WildLifeTracker.Models
+{
+    [Table("species")]
+    public class Species
+    {
+        [Key]
+        public int speciesId { get; set; }
+        public string name { get; set; }
+    }
+}
